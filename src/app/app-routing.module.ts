d6501ec14a20406a6519e5d0121c0453b52dc766
zenger4000 <89679647+zenger4000@users.noreply.github.com>
2022-03-15 22:41:14 +0200

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PostsComponent } from './posts/posts.component';
+import { ProductsComponent } from './products/products.component';
+import { RegisterComponent } from './register/register.component';
+import { UsersComponent } from './users/users.component';
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'products',component:ProductsComponent},
+  {path:'users',component:UsersComponent},
+  {path:'posts',component:PostsComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'login',component:LoginComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
